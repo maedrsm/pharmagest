651de05d68362da3bf442ffadb01eda1dd6dfd0c
maedrsm <131335311+maedrsm@users.noreply.github.com>
2024-06-09 20:04:42 +0400

    Delete codes directory

--- codes/src/main/java/module-info.java
+++
@@ -1,9 +0,0 @@
-module com.example.pharma_gest {
-    requires javafx.controls;
-    requires javafx.fxml;
-    requires java.sql;
-
-    opens com.example.pharma_gest to javafx.fxml;
-    exports com.example.pharma_gest;
-}
-
